157fe542c0b3f16a48604328138a96a87253dcc2
Manjeet Raj <manjeetraj@Manjeets-MacBook-Air.local>
2026-05-24 00:04:39 +0530

    fix scheduling

--- src/main/java/com/fittrack/analytics_core/AnalyticsCoreApplication.java
+++ src/main/java/com/fittrack/analytics_core/AnalyticsCoreApplication.java
@@ -2,8 +2,10 @@ package com.fittrack.analytics_core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling; // 🎯 FIXED: Added missing import
 
 @SpringBootApplication
+@EnableScheduling //
 public class AnalyticsCoreApplication {
 
 	public static void main(String[] args) {
